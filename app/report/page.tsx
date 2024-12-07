3aa520449bb6b98123e4c27fc74d77998012bb37
choidabom <dabomi0305@naver.com>
2024-12-07 17:50:18 +0900

    feat: 차트 UI 추가

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,6 +1,14 @@
+'use client';
 import arrowLeftIcon from '@/public/report/icon/chevron-left.svg';
 import computerIcon from '@/public/report/icon/code-computer.svg';
 import Image from 'next/image';
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  ChartConfig,
+} from '@/components/ui/chart';
+import { Bar, BarChart, LabelList, Pie, PieChart } from 'recharts';
 
 let num = 0;
 const listItem = [
@@ -32,6 +40,60 @@ const listItem = [
     title: 'Apple 2025 맥북프로 16 (M4)',
     price: 5000000,
   },
+];
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: '#ff0',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: '#ff0000',
+  },
+} satisfies ChartConfig;
+
+const chartConfig2 = {
+  visitors: {
+    label: 'Visitors',
+  },
+  chrome: {
+    label: 'Chrome',
+    color: 'hsl(var(--chart-1))',
+  },
+  safari: {
+    label: 'Safari',
+    color: 'hsl(var(--chart-2))',
+  },
+  firefox: {
+    label: 'Firefox',
+    color: 'hsl(var(--chart-3))',
+  },
+  edge: {
+    label: 'Edge',
+    color: 'hsl(var(--chart-4))',
+  },
+  other: {
+    label: 'Other',
+    color: 'hsl(var(--chart-5))',
+  },
+} satisfies ChartConfig;
+
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+];
+
+const chartData2 = [
+  { browser: 'chrome', visitors: 275, fill: 'var(--color-chrome)' },
+  { browser: 'safari', visitors: 200, fill: '' },
+  { browser: 'firefox', visitors: 187, fill: 'var(--color-firefox)' },
+  { browser: 'edge', visitors: 173, fill: 'var(--color-edge)' },
+  { browser: 'other', visitors: 90, fill: 'var(--color-other)' },
 ];
 
 const ReportPage = () => {
@@ -83,12 +145,42 @@ const ReportPage = () => {
         </ul>
       </div>
 
-      {/* 차트 */}
-      <div></div>
+      {/** 총액 / 퍼센트 */}
       <div className='px-[40px] py-[24px]'>
         <div className='flex items-center justify-end gap-[30px]'>
           <div className='text-[20px]'>총액: {totalPrice.toLocaleString()}원</div>
           <div className='text-[20px] p-[8px]'>{((totalPrice / lottoPrice) * 100).toFixed(1)}%</div>
+        </div>
+      </div>
+
+      {/* 차트 */}
+      <div>
+        <div>
+          <ChartContainer config={chartConfig} className='min-h-[200px] w-full'>
+            <BarChart accessibilityLayer data={chartData}>
+              <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
+              <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+            </BarChart>
+          </ChartContainer>
+        </div>
+        <div>
+          <ChartContainer
+            config={chartConfig2}
+            className='mx-auto aspect-square max-h-[250px] [&_.recharts-text]:fill-background'
+          >
+            <PieChart>
+              <ChartTooltip content={<ChartTooltipContent nameKey='visitors' hideLabel />} />
+              <Pie data={chartData2} dataKey='visitors'>
+                <LabelList
+                  dataKey='browser'
+                  className='fill-background'
+                  stroke='none'
+                  fontSize={12}
+                  formatter={(value: keyof typeof chartConfig2) => chartConfig2[value]?.label}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
         </div>
       </div>
     </main>
