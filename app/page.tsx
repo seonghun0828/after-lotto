8e25b5cf7de41fe56cef9e913d003b0a4b9574fe
jojo <pch468@naver.com>
2024-12-07 12:14:37 +0900

    chore: eslint and prettier setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import React, { useState } from 'react';
+
 export default function Home() {
+  const [lottoNumbers, setLottoNumbers] = useState<number[]>([]);
+
   return (
     <div>
       <h1>Hello World</h1>
+      <div>hihihi</div>
     </div>
   );
 }
