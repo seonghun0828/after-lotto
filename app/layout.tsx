219485a3604d12d4dca9d706884420afb0e54254
gayoung <gayoungyeom@gmail.com>
2024-12-07 16:56:20 +0900

    feat: global layout stlye

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'After Lotto',
+  description: 'Welcome to After Lotto',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='w-screen flex justify-center'>
+        <div className='h-screen w-[600px] min-w-[320px] bg-black '>{children}</div>
+      </body>
+    </html>
+  );
+}
