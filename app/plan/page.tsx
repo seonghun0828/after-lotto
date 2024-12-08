1801d27b0b186803c5b22f9deea4471d4eb938c8
gayoung <gayoungyeom@gmail.com>
2024-12-08 21:30:24 +0900

    fix: 충돌 해결

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -3,19 +3,15 @@
 import PriceInfo from './_components/PriceInfo';
 import BottomSheet from './_components/BottomSheet';
 import AddedItemList from './_components/AddedItemList';
-import { IItem } from './_types/plan.types';
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import ShareButton from './_components/ShareButton';
 
 const Plan = () => {
-  const [, setSelectedItems] = useState<IItem[]>([]);
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className='font-pretendard text-small'>
         <div className='p-8'>
           <ShareButton />
-
           <PriceInfo />
         </div>
 
