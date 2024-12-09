86f4cdaa17177811cef07f99eebdf4fc928268a0
choidabom <dabomi0305@naver.com>
2024-12-09 22:53:04 +0900

    fix: 퍼센트 계산 수정 및 아이템 없는 경우 공유되지 않도록 수정

--- app/plan/_hooks/useMoveToReport.ts
+++ app/plan/_hooks/useMoveToReport.ts
@@ -1,3 +1,4 @@
+import { toast } from '@/app/_hooks/useToast';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 export const useMoveToReport = () => {
@@ -6,6 +7,15 @@ export const useMoveToReport = () => {
   const items = searchParams.get('items');
 
   const handleShareClick = () => {
+    if (items === null) {
+      toast({
+        className: 'fixed top-2 left-1/2 z-[100] -translate-x-1/2 w-full max-w-md p-4',
+        variant: 'destructive',
+        title: '아이템을 담아주세요!',
+        duration: 1000,
+      });
+      return;
+    }
     router.push(`/report?items=${items}`);
   };
 
