da400dc0a621f07459ba79332371ef804c89ba9d
kbs21c1987 <kbs21c1987@gmail.com>
2024-12-09 19:51:38 +0900

    fix: 리포트 당첨 금액 1등 당첨금으로 변경

--- app/_types/lotto.type.ts
+++ app/_types/lotto.type.ts
@@ -1,16 +1,16 @@
 export interface LottoType {
-  totSellamnt: number;
-  returnValue: 'success' | 'fail';
-  drwNoDate: string;
-  firstWinamnt: number;
-  firstPrzwnerCo: number;
-  bnusNo: number;
-  firstAccumamnt: number;
-  drwNo: number;
-  drwtNo1: number;
-  drwtNo2: number;
-  drwtNo3: number;
-  drwtNo4: number;
-  drwtNo5: number;
-  drwtNo6: number;
+  totSellamnt: number; // 총 판매금액
+  returnValue: 'success' | 'fail'; // 결과
+  drwNoDate: string; // 추첨일
+  firstWinamnt: number; // 1등 당첨금
+  firstPrzwnerCo: number; // 1등 당첨 인원
+  bnusNo: number; // 보너스 번호
+  firstAccumamnt: number; // 1등 당첨금 총액
+  drwNo: number; // 회차
+  drwtNo1: number; // 당첨번호 1
+  drwtNo2: number; // 당첨번호 2
+  drwtNo3: number; // 당첨번호 3
+  drwtNo4: number; // 당첨번호 4
+  drwtNo5: number; // 당첨번호 5
+  drwtNo6: number; //
 }
